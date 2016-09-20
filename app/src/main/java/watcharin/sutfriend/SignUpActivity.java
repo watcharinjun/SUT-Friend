d880863526e3ff4129b2f729b0d32986aa8add1a
watchirin <junwatcharin1992@gmail.com>
2016-09-20 16:39:31 +0700

    nead

--- app/src/main/java/watcharin/sutfriend/SignUpActivity.java
+++ app/src/main/java/watcharin/sutfriend/SignUpActivity.java
@@ -9,5 +9,5 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
+    } // Main Metthod
 }
